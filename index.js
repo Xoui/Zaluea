fd2b7226b758efa2e01243256187a5789dd21739
Darwin Skenderasi <shivermytimbers123@outlook.com>
2023-11-13 22:09:40 +0000

    ban

--- index.js
+++ index.js
@@ -16,18 +16,61 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
+const admins = [
+  { username: 'admin', password: 'admin123' },
+  // Add other admin accounts as needed
+];
+
+const bannedUsers = [];
+
+// Function to check if a user is banned
+function isUserBanned(username) {
+  return bannedUsers.includes(username);
+}
+
+// Function to ban a user
+function banUser(username) {
+  if (!isUserBanned(username)) {
+    bannedUsers.push(username);
+    io.emit('chat message', `[Server]: ${username} has been banned.`);
+  }
+}
+
+// Function to get the list of banned users
+function getBanList() {
+  return bannedUsers;
+}
+
 io.on('connection', (socket) => {
-  socket.on('username', (username) => {
-    socket.username = username;
-    io.emit('chat message', `[Server]: ${username} has joined the chat.`);
+  socket.on('chat message', (msg) => {
+    // Check if the message is a ban command
+    if (msg.startsWith('/ban')) {
+      // Extract the username from the message
+      const [, usernameToBan] = msg.split(' ');
+
+      if (socket.isAdmin) {
+        // Only admins can use the ban command
+        banUser(usernameToBan);
+      } else {
+        io.emit('chat message', `[Server]: Only admins can use the /ban command.`);
+      }
+    } else if (msg === '/banlist' && socket.isAdmin) {
+      // Check if the message is a banlist command
+      const banList = getBanList();
+      io.emit('chat message', `[Server]: Banned users: ${banList.join(', ')}`);
+    } else if (isUserBanned(socket.username)) {
+      // If the user is banned, prevent them from sending messages
+      socket.emit('chat message', '[Server]: You have been banned.');
+    } else {
+      // Regular chat message
+      io.emit('chat message', `${socket.username || 'Anonymous'}: ${msg}`);
+    }
   });
 
   socket.on('disconnect', () => {
-    io.emit('chat message', `[Server]: ${socket.username} has left the chat.`);
-  });
-
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', `${socket.username}: ${msg}`);
+    if (socket.username) {
+      io.emit('chat message', `[Server]: ${socket.username} has left the chat.`);
+    }
   });
 
   socket.on('rename', (newName) => {
